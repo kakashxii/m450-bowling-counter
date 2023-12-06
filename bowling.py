7341ef44428cf0bae505cf1767f71c49ab5b63f6
lilly kate <71640994+Lilly05@users.noreply.github.com>
2023-12-06 13:16:04 +0100

    Update bowling.py

--- bowling.py
+++ bowling.py
@@ -30,33 +30,67 @@ def bowling_counter():
         if first_throw > 10 or first_throw < 0:
             print("Invalid input. Please enter a number between 0 and 10.")
             continue
-        
+
         print_pins(first_throw)
 
-        if first_throw == 10:
+        if first_throw == 10 and frame < 10:
             print("Strike!")
             score += 10
             frame += 1
             continue
+        elif first_throw == 10 and frame == 10:
+            print("Strike!")
+            score += 10
+
+            # Second throw in 10th frame
+            second_throw = int(input("Enter the pins overturned in the second throw: "))
+            print_pins(second_throw)
+            score += second_throw
+
+            if second_throw == 10:  # Player gets two additional throws
+                print("Strike!")
+                score += 10
+
+                # Third throw in 10th frame
+                third_throw = int(input("Enter the pins overturned in the third throw: "))
+                print_pins(third_throw)
+                score += third_throw
+
+            frame += 1
+            continue
+        elif first_throw == 0:
+            print('Miss!')
 
         second_throw = int(input("Enter the pins overturned in the second throw: "))
 
         if second_throw > 10 or second_throw < 0 or (first_throw + second_throw) > 10:
             print("Invalid input. Please enter a number between 0 and 10.")
             continue
-        
+
         print_pins(second_throw)
         frame_score = first_throw + second_throw
         score += frame_score
 
-        if frame_score == 10: #Spare
+        if frame_score == 10:  # Spare
             print("Spare!")
+            if frame == 10:
+                # Bonus throw for spare in 10th frame
+                bonus_throw = int(input("Enter the pins overturned in the bonus throw: "))
+                print_pins(bonus_throw)
+                score += bonus_throw
+        elif frame_score == first_throw:
+            print('Miss!')
+
         elif frame_score < 10:
             print("Open Frame!")
 
         frame += 1
-
+    
+    if score == 130:
+        print('Perfect game!')
+        score = 300
     print(f"Total score: {score}")
+
 
 ascii_art = """
 
